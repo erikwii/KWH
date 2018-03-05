192bab97a5f1db3558b2f6563443252ce9a6265a
Erik Santiago <eriksantiago.science@gmail.com>
2018-03-05 15:31:17 +0700

    add kwh sql v 1.0.0
    please make database on xampp named kwh
    import kwh 1.0.0.sql to your db

---
+++ sql/kwh 1.0.0.sql
@@ -0,0 +1,434 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 05, 2018 at 09:25 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kwh`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `IDadmin` int(11) NOT NULL,
+  `namaAdmin` varchar(50) NOT NULL,
+  `IDusers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `app_setting`
+--
+
+CREATE TABLE `app_setting` (
+  `IDappSetting` int(11) NOT NULL,
+  `field` varchar(50) NOT NULL,
+  `value` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyer`
+--
+
+CREATE TABLE `buyer` (
+  `IDbuyer` int(11) NOT NULL,
+  `IDusers` int(11) NOT NULL,
+  `namaBuyer` varchar(50) NOT NULL,
+  `fakultas` varchar(50) NOT NULL,
+  `prodi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `IDcategory` int(11) NOT NULL,
+  `namaCategory` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chart`
+--
+
+CREATE TABLE `chart` (
+  `IDchart` int(11) NOT NULL,
+  `IDinvoice` int(11) NOT NULL,
+  `IDproduct` int(11) NOT NULL,
+  `jumlahProduct` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice`
+--
+
+CREATE TABLE `invoice` (
+  `IDinvoice` int(11) NOT NULL,
+  `besaran` int(11) NOT NULL,
+  `alamatKirim` varchar(255) NOT NULL,
+  `ongkir` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `IDproduct` int(11) NOT NULL,
+  `namaProduct` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `deskripsiProduct` varchar(11) DEFAULT NULL,
+  `IDseller` int(11) NOT NULL,
+  `IDcategory` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `minimum` int(5) NOT NULL,
+  `photo1` varchar(255) NOT NULL,
+  `photo2` varchar(255) DEFAULT NULL,
+  `photo3` varchar(255) DEFAULT NULL,
+  `photo4` varchar(255) DEFAULT NULL,
+  `photo5` varchar(255) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_rate`
+--
+
+CREATE TABLE `product_rate` (
+  `IDproductRate` int(11) NOT NULL,
+  `rate` int(5) NOT NULL,
+  `IDbuyer` int(11) NOT NULL,
+  `IDproduct` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promo`
+--
+
+CREATE TABLE `promo` (
+  `IDpromo` int(11) NOT NULL,
+  `namaPromo` varchar(50) NOT NULL,
+  `deskripsiPromo` varchar(255) DEFAULT NULL,
+  `potongan` int(11) NOT NULL,
+  `status` enum('Tidak Aktif','Aktif') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `IDseller` int(11) NOT NULL,
+  `IDusers` int(11) NOT NULL,
+  `namaSeller` varchar(50) NOT NULL,
+  `deskripsiSeller` varchar(255) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `rate` float DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller_rate`
+--
+
+CREATE TABLE `seller_rate` (
+  `IDsellerRate` int(11) NOT NULL,
+  `rate` int(5) NOT NULL,
+  `IDbuyer` int(11) NOT NULL,
+  `IDseller` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `IDtransaction` int(11) NOT NULL,
+  `IDbuyer` int(11) NOT NULL,
+  `IDseller` int(11) NOT NULL,
+  `IDinvoice` int(11) NOT NULL,
+  `IDpromo` int(11) DEFAULT NULL,
+  `tanggal` date NOT NULL,
+  `waktu` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `IDusers` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` int(13) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`IDadmin`),
+  ADD KEY `IDusers_users_admin` (`IDusers`);
+
+--
+-- Indexes for table `app_setting`
+--
+ALTER TABLE `app_setting`
+  ADD PRIMARY KEY (`IDappSetting`);
+
+--
+-- Indexes for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD PRIMARY KEY (`IDbuyer`),
+  ADD KEY `IDusers_users_buyer` (`IDusers`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`IDcategory`);
+
+--
+-- Indexes for table `chart`
+--
+ALTER TABLE `chart`
+  ADD PRIMARY KEY (`IDchart`),
+  ADD KEY `IDinvoice_invoice_chart` (`IDinvoice`),
+  ADD KEY `IDproduct_product_chart` (`IDproduct`);
+
+--
+-- Indexes for table `invoice`
+--
+ALTER TABLE `invoice`
+  ADD PRIMARY KEY (`IDinvoice`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`IDproduct`),
+  ADD KEY `IDseller_seller_product` (`IDseller`),
+  ADD KEY `IDcategory_category_product` (`IDcategory`);
+
+--
+-- Indexes for table `product_rate`
+--
+ALTER TABLE `product_rate`
+  ADD PRIMARY KEY (`IDproductRate`),
+  ADD KEY `IDbuyer_buyer_product_rate` (`IDbuyer`),
+  ADD KEY `IDproduct_product_product_rate` (`IDproduct`);
+
+--
+-- Indexes for table `promo`
+--
+ALTER TABLE `promo`
+  ADD PRIMARY KEY (`IDpromo`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`IDseller`),
+  ADD KEY `IDusers_users_seller` (`IDusers`);
+
+--
+-- Indexes for table `seller_rate`
+--
+ALTER TABLE `seller_rate`
+  ADD PRIMARY KEY (`IDsellerRate`),
+  ADD KEY `IDbuyer_buyer_seller_rate` (`IDbuyer`),
+  ADD KEY `IDseller_seller_seller_rate` (`IDseller`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`IDtransaction`),
+  ADD KEY `IDbuyer_buyer_transaction` (`IDbuyer`),
+  ADD KEY `IDseller_seller_transaction` (`IDseller`),
+  ADD KEY `IDinvoice_invoice_transaction` (`IDinvoice`),
+  ADD KEY `IDpromo_promo_transaction` (`IDpromo`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`IDusers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `IDadmin` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `app_setting`
+--
+ALTER TABLE `app_setting`
+  MODIFY `IDappSetting` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `buyer`
+--
+ALTER TABLE `buyer`
+  MODIFY `IDbuyer` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `IDcategory` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `chart`
+--
+ALTER TABLE `chart`
+  MODIFY `IDchart` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `invoice`
+--
+ALTER TABLE `invoice`
+  MODIFY `IDinvoice` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `IDproduct` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_rate`
+--
+ALTER TABLE `product_rate`
+  MODIFY `IDproductRate` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `promo`
+--
+ALTER TABLE `promo`
+  MODIFY `IDpromo` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `IDseller` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `seller_rate`
+--
+ALTER TABLE `seller_rate`
+  MODIFY `IDsellerRate` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `IDtransaction` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `IDusers` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `IDusers_users_admin` FOREIGN KEY (`IDusers`) REFERENCES `users` (`IDusers`);
+
+--
+-- Constraints for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD CONSTRAINT `IDusers_users_buyer` FOREIGN KEY (`IDusers`) REFERENCES `users` (`IDusers`);
+
+--
+-- Constraints for table `chart`
+--
+ALTER TABLE `chart`
+  ADD CONSTRAINT `IDinvoice_invoice_chart` FOREIGN KEY (`IDinvoice`) REFERENCES `invoice` (`IDinvoice`),
+  ADD CONSTRAINT `IDproduct_product_chart` FOREIGN KEY (`IDproduct`) REFERENCES `product` (`IDproduct`);
+
+--
+-- Constraints for table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `IDcategory_category_product` FOREIGN KEY (`IDcategory`) REFERENCES `category` (`IDcategory`),
+  ADD CONSTRAINT `IDseller_seller_product` FOREIGN KEY (`IDseller`) REFERENCES `seller` (`IDseller`);
+
+--
+-- Constraints for table `product_rate`
+--
+ALTER TABLE `product_rate`
+  ADD CONSTRAINT `IDbuyer_buyer_product_rate` FOREIGN KEY (`IDbuyer`) REFERENCES `buyer` (`IDbuyer`),
+  ADD CONSTRAINT `IDproduct_product_product_rate` FOREIGN KEY (`IDproduct`) REFERENCES `product` (`IDproduct`);
+
+--
+-- Constraints for table `seller`
+--
+ALTER TABLE `seller`
+  ADD CONSTRAINT `IDusers_users_seller` FOREIGN KEY (`IDusers`) REFERENCES `users` (`IDusers`);
+
+--
+-- Constraints for table `seller_rate`
+--
+ALTER TABLE `seller_rate`
+  ADD CONSTRAINT `IDbuyer_buyer_seller_rate` FOREIGN KEY (`IDbuyer`) REFERENCES `buyer` (`IDbuyer`),
+  ADD CONSTRAINT `IDseller_seller_seller_rate` FOREIGN KEY (`IDseller`) REFERENCES `seller` (`IDseller`);
+
+--
+-- Constraints for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD CONSTRAINT `IDbuyer_buyer_transaction` FOREIGN KEY (`IDbuyer`) REFERENCES `buyer` (`IDbuyer`),
+  ADD CONSTRAINT `IDinvoice_invoice_transaction` FOREIGN KEY (`IDinvoice`) REFERENCES `invoice` (`IDinvoice`),
+  ADD CONSTRAINT `IDpromo_promo_transaction` FOREIGN KEY (`IDpromo`) REFERENCES `promo` (`IDpromo`),
+  ADD CONSTRAINT `IDseller_seller_transaction` FOREIGN KEY (`IDseller`) REFERENCES `seller` (`IDseller`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
